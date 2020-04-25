94a4fe00f60be77a59eb1446bc05770d210bd37f
Roberto Martinez <hdkrus@gmail.com>
2020-04-24 23:20:24 -0400

    add test assertions and fixes

--- kladama_test.py
+++ kladama_test.py
@@ -11,77 +11,94 @@ def get_sandbox_session():
 
 
 def test_areas_of_interest():
+    print('Testing Areas of Interest ========================')
+
     session = get_sandbox_session()
     aois = kl.catalog(session).areas_of_interest
-
-    print('Areas of Interest ========================')
+    assert len(aois) > 0
     for aoi in aois:
+        assert isinstance(aoi, kl.AreaOfInterest)
         print(aoi.name, '-', aoi.description, 'in', aoi.link)
 
     print('\n')
 
 
 def test_areas_of_interest_for_dev():
+    print('Testing Areas of Interest for dev ========================')
+
     session = get_sandbox_session()
     aois = kl.catalog(session).areas_of_interest_for_user('dev')
-
-    print('Areas of Interest for dev ========================')
+    assert len(aois) > 0
     for aoi in aois:
+        assert isinstance(aoi, kl.AreaOfInterest)
         print(aoi.name, '-', aoi.description, 'in', aoi.link)
 
     print('\n')
 
 
 def test_variables():
+    print('Testing Variables ========================')
+
     session = get_sandbox_session()
     variables = kl.catalog(session).vars
-
-    print('Variables ========================')
+    assert len(variables) > 0
     for var in variables:
+        assert isinstance(var, kl.Variable)
         print(var.name, 'from', var.source.name, 'in', var.link)
 
     print('\n')
 
 
 def test_sources():
+    print('Testing Sources ========================')
+
     session = get_sandbox_session()
     sources = kl.catalog(session).src
-
-    print('Sources ========================')
+    assert len(sources) > 0
     for source in sources:
+        assert isinstance(source, kl.Source)
         print(source.name, ':', source.description, 'in', source.link)
 
     print('\n')
 
 
 def test_phenoms():
+    print('Testing Phenoms ========================')
+
     session = get_sandbox_session()
     phenoms = kl.catalog(session).phenom
-
-    print('Phenoms ========================')
+    assert len(phenoms) > 0
     for phenom in phenoms:
+        assert isinstance(phenom, kl.Phenomenon)
         print(phenom.name, ':', phenom.description, 'in', phenom.link)
 
     print('\n')
 
 
 def test_organizations():
+    print('Testing Organizations ========================')
+
     session = get_sandbox_session()
     organizations = kl.catalog(session).organizations
-
-    print('Organizations ========================')
+    assert len(organizations) > 0
     for organization in organizations:
-        print(organization.name, ':', organization.acronym, 'in', organization.link)
+        assert isinstance(organization, kl.Organization)
+        print(
+            organization.name, ':', organization.acronym,
+            'in', organization.link
+        )
 
     print('\n')
 
 
 def test_users():
+    print('Testing Users ========================')
+
     session = get_sandbox_session()
     users = kl.catalog(session).users
-
-    print('Users ========================')
+    assert len(users) > 0
     for user in users:
+        assert isinstance(user, kl.User)
         print(user.username, 'from', user.organization.name, 'in', user.link)
 
     print('\n')
