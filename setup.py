78394d9052b2e82f0bf1b194504e525c503ff6a1
Roberto Martinez <hdkrus@gmail.com>
2020-07-24 09:59:28 -0400

    release v1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="kladama-api", # Replace with your own username
-    version="1.0.3",
+    version="1.0.4",
     author="Plexilar",
     author_email="hdkrus@gmail.com",
     description="Kladama API package",
